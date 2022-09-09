9e77bd27fef6b1a2375a5698cefe414fa2b7984e
pilipalabow <pilipalabow@gmail.com>
2022-09-09 08:09:18 +0800

    加入註解，完成題目

--- Convert Celsius to Fahrenheit/Convert_Celsius_to_Fahrenheit.js
+++ Convert Celsius to Fahrenheit/Convert_Celsius_to_Fahrenheit.js
@@ -1,5 +1,7 @@
+// 轉換攝氏溫度成華氏溫度
 function convertCtoF(celsius) {
-    let fahrenheit;
+    // The formula to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+    let fahrenheit = celsius * 9 / 5 + 32;
     return fahrenheit;
   }
   
